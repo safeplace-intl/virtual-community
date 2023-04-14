13e2af03cf03da968dfb71b3771e6bd8774556e4
Eleni Papanicolas <elenipapanicolas@gmail.com>
2023-04-14 17:25:08 -0400

    Add client packages

--- src/client/src/main.tsx
+++ src/client/src/main.tsx
@@ -1,10 +1,41 @@
+import "./index.css";
+
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import App from "./App";
-import "./index.css";
+
+const httpLink = createHttpLink({
+  uri: `http://localhost:8081`,
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+export const client = new ApolloClient({
+  uri: `localhost:8081`,
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
