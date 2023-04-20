91ea8a757a50c8c19205be48298ad3dc7fd6d142
sk <jsckhan@outlook.com>
2023-04-20 14:00:00 -0400

    feat: update vite config for prod and dev

--- src/client/vite.config.ts
+++ src/client/vite.config.ts
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
 
+let viteConfig = {};
+
+if (process.env.NODE_ENV === "production") {
+  viteConfig = {
+    plugins: [preact()],
+    build: {
+      outDir: "../../build/client/dist",
+      manifest: true,
+      rollupOptions: {
+        input: "index.html",
+      },
+    },
+    server: {
+      origin: "http://127.0.0.1:8081",
+    },
+  };
+} else {
+  viteConfig = {
+    plugins: [preact()],
+    build: {
+      outDir: "./dist",
+      manifest: true,
+      rollupOptions: {
+        input: "./src/main.tsx",
+      },
+    },
+    server: {
+      port: 3001,
+      origin: "http://127.0.0.1:8081",
+    },
+  };
+}
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [preact()],
-})
+export default defineConfig(viteConfig);
