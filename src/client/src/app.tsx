620c23e048327c8516835c97a5a84606dc2f6575
ssjkhan <114552289+ssjkhan@users.noreply.github.com>
2023-04-20 16:22:58 -0400

    Min App Template
    * Init contribution workflow (#8)
    
    * Update contributions guidance
    
    * Add typescript error extension
    
    * Create .env example with fake strings
    
    * feat: remove react testing client
    
    * feat: base preact vite client
    
    * feat: update vite config for prod and dev
    
    * feat: install dep for minimal client
    
    tailwind css, postcss, autoprefixer, wouter
    
    * feat: tailwindcss init
    
    * feat: remove boiler plate and add base
    
    ---------
    
    Co-authored-by: Eleni Papanicolas <76261375+e-papanicolas@users.noreply.github.com>

---
+++ src/client/src/app.tsx
@@ -0,0 +1,11 @@
+import {} from "preact/hooks";
+
+export function App() {
+  return (
+    <div className="bg-red-500 h-screen w-screen m-auto flex">
+      <div className="w-fit h-fit text-white text-2xl m-auto pb-32">
+        Clean App
+      </div>
+    </div>
+  );
+}
