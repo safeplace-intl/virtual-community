1f7c6a7326ffe4fef053144b5e659937df7a2288
sk <jsckhan@outlook.com>
2023-04-20 14:44:39 -0400

    feat: remove boiler plate and add base

--- src/client/src/app.tsx
+++ src/client/src/app.tsx
@@ -1,33 +1,11 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
-import './app.css'
+import {} from "preact/hooks";
 
 export function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
+    <div className="bg-red-500 h-screen w-screen m-auto flex">
+      <div className="w-fit h-fit text-white text-2xl m-auto pb-32">
+        Clean App
       </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
