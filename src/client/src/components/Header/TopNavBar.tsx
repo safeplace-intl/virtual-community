7816413d61db2c89cb410fd9d63d5ead63ec4825
aerlikh <aerlikh2011@gmail.com>
2023-05-02 19:16:51 +0000

    changes

--- src/client/src/components/Header/TopNavBar.tsx
+++ src/client/src/components/Header/TopNavBar.tsx
@@ -1,8 +1,15 @@
 import logo from "@assets/spi-logo.png";
+import { h } from "preact";
 import { useState } from "preact/hooks";
 
 interface TopNavBarProps {
   onTopMenuItemClick: (menuItem: string) => void;
+}
+
+interface MenuItem {
+  name: string;
+  onClick: string;
+  icon?: h.JSX.Element;
 }
 
 export default function TopNavBar({ onTopMenuItemClick }: TopNavBarProps) {
@@ -10,15 +17,31 @@ export default function TopNavBar({ onTopMenuItemClick }: TopNavBarProps) {
 
   const toggleMenu = () => setIsOpen(!isOpen);
 
-  const menuItems = [
-    { name: "Feed", onClick: "Feed" },
-    { name: "Reports", onClick: "Reports" },
-    { name: "Profile", onClick: "Profile" },
-    { name: "Settings", onClick: "Settings" },
+  const menuItems: MenuItem[] = [
+    {
+      name: "Feed",
+      onClick: "Feed",
+      // icon: <MdInbox size={24} aria-hidden="true" focusable="false" />,
+    },
+    {
+      name: "Reports",
+      onClick: "Reports",
+      // icon: <MdPieChart size={24} aria-hidden="true" focusable="false" />,
+    },
+    {
+      name: "Profile",
+      onClick: "Profile",
+      // icon: <MdPerson size={24} aria-hidden="true" focusable="false" />,
+    },
+    {
+      name: "Settings",
+      onClick: "Settings",
+      // icon: <MdSettings size={24} aria-hidden="true" focusable="false" />,
+    },
   ];
 
   return (
-    <nav className="sticky top-0 z-50 flex justify-between py-0 px-5 md:px-10 h-16 w-full bg-modal-100 border border-black-100 shadow box-shadow-[0_4px_4px_0px]-black-100">
+    <nav className="sticky top-0 z-50 flex justify-between py-0 px-100 md:px-10 h-64px w-1440px bg-modal-100 border border-black-100 shadow box-shadow-[0_4px_4px_0px]-black-100">
       <div className="flex items-center">
         <a href="#">
           <img src={logo} alt="SPI Logo" className="h-8 mr-4"></img>
@@ -30,7 +53,7 @@ export default function TopNavBar({ onTopMenuItemClick }: TopNavBarProps) {
           <div className="relative">
             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
               <svg
-                className="w-5 h-5 text-gray-500"
+                className="w-5 h-5 text-gray-500 text-4xl font-bold leading-56 tracking-wide"
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
@@ -55,18 +78,18 @@ export default function TopNavBar({ onTopMenuItemClick }: TopNavBarProps) {
       </div>
 
       {/* Desktop Menu */}
-      <div className="hidden md:flex items-center text-black-100 py-2">
+      <div className="hidden md:flex items-center text-black-100 py-2 gap-4">
         {menuItems.map((item) => (
           <a
             key={item.name}
             onClick={() => onTopMenuItemClick(item.onClick)}
             href="#"
-            className="font-bold py-2 px-4 hover:underline hover:text-spi-violet-100"
+            className="text-base font-normal leading-6 tracking-normal py-2 px-4 hover:underline hover:text-spi-violet-100"
           >
             {item.name}
           </a>
         ))}
-        <button className="bg-modal-100 text-spi-violet-100 font-bold py-2 px-4 border rounded-lg border-spi-violet-100 mr-2">
+        <button className="text-base font-bold leading-6 tracking-normal bg-modal-100 text-spi-violet-100 py-2 px-4 border rounded-lg border-spi-violet-100 mr-2">
           Log In
         </button>
       </div>
@@ -98,7 +121,7 @@ export default function TopNavBar({ onTopMenuItemClick }: TopNavBarProps) {
                 {item.name}
               </a>
             ))}
-            <button className="w-full text-left bg-modal-100 text-spi-violet-100 font-bold py-2 px-4 border rounded-lg border-spi-violet-100">
+            <button className="font-bold w-full text-left bg-modal-100 text-spi-violet-100 py-2 px-4 border rounded-lg border-spi-violet-100">
               Log In
             </button>
           </div>
