70722de3686c82b66905a8a40d56b14fa3e0f2f6
RGibbs96 <106614039+RGibbs96@users.noreply.github.com>
2023-05-09 11:33:27 -0700

    R gibbs96/create profile module (#27)
    * created profile resolver and services, as well as dto and entity to allow for mutations
    
    ---------
    
    Co-authored-by: Ryan Gibbs <ryangibbs@Ryans-MacBook-Air.local>
    Co-authored-by: Eleni Papanicolas <elenipapanicolas@gmail.com>

---
+++ src/modules/profile/profile.service.ts
@@ -0,0 +1,59 @@
+import { Profile } from "@prisma/client";
+import { Service } from "typedi";
+
+import {
+  CreateProfileInput,
+  UpdateProfileInput,
+} from "../../core/dto/profile.dto.js";
+import { prisma } from "../../prisma/index.js";
+
+@Service()
+export default class ProfileService {
+  async getProfileByUserId(userId: number): Promise<Profile> {
+    const profile = await prisma.profile.findUnique({
+      where: { userId },
+    });
+
+    if (!profile) {
+      throw new Error("Profile not found");
+    }
+
+    return profile;
+  }
+
+  async createProfile(
+    userId: number,
+    profileInput: CreateProfileInput
+  ): Promise<Profile> {
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+
+    if (!user) {
+      throw new Error("User not found");
+    } else {
+      return await prisma.profile.create({
+        data: {
+          userId: userId,
+          fullName: profileInput.fullName,
+          pronouns: profileInput.pronouns,
+          tdaGradYear: profileInput.tdaGradYear,
+          currentLocation: profileInput.currentLocation,
+          bio: profileInput.bio,
+        },
+      });
+    }
+  }
+
+  async updateProfile(
+    userId: number,
+    profileUpdateInput: UpdateProfileInput
+  ): Promise<Profile> {
+    const profile = await prisma.profile.update({
+      where: { userId },
+      data: profileUpdateInput,
+    });
+
+    return profile;
+  }
+}
