5e58c690d32a1b7c9f275f1f74c42eeb0962f15d
sk <jsckhan@outlook.com>
2023-05-31 00:23:57 -0400

    feat: testing route for display

--- src/client/src/app.tsx
+++ src/client/src/app.tsx
@@ -1,11 +1,20 @@
+import Testing from "@features/testing";
 import {} from "preact/hooks";
+import { Route } from "wouter-preact";
 
 export function App() {
   return (
-    <div className="bg-red-500 h-screen w-screen m-auto flex">
-      <div className="w-fit h-fit text-white text-2xl m-auto pb-32">
-        Clean App
-      </div>
-    </div>
+    <>
+      <Route path="/">
+        <div className="bg-red-500 h-screen w-screen m-auto flex">
+          <div className="w-fit h-fit text-white text-2xl m-auto pb-32">
+            Clean App
+          </div>
+        </div>
+      </Route>
+      <Route path="/testing">
+        <Testing />
+      </Route>
+    </>
   );
 }
