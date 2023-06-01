c1a927efe5ea5c67031035c70dcca38f72801878
Eleni Papanicolas <elenipapanicolas@gmail.com>
2023-06-01 17:24:53 -0400

    Fix database abstraction, add try/catch to services

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -28,100 +28,115 @@ export default class UserService implements IUserService {
   }
 
   async getUserById(userId: number): Promise<User> {
-    const user = await this.databaseService.users.findUnique({
-      where: { id: userId },
-    });
+    try {
+      const user = await this.databaseService.users.findUnique({
+        where: { id: userId },
+      });
 
-    if (!user) {
-      throw new Error("User not found");
+      if (!user) {
+        throw new Error("User not found");
+      }
+
+      return user;
+    } catch (error) {
+      throw new Error((error as Error).message);
     }
-
-    return user;
   }
 
   async createUser(userInput: CreateUserInput): Promise<User> {
     const email = userInput.email;
 
-    // checks to make sure email is not already in use
-    const existingUser = await this.databaseService.users.findUnique({
-      where: { email },
-    });
-
-    if (existingUser) {
-      throw new Error("Email already in use.");
-    } else {
-      const password = await this.authService.createPassword(
-        userInput.password
-      );
-
-      // returns the newly created user object to the resolver
-      const user = await this.databaseService.users.create({
-        data: {
-          email: userInput.email,
-          passwordHash: password,
-          isActive: true,
-        },
+    try {
+      // checks to make sure email is not already in use
+      const existingUser = await this.databaseService.users.findUnique({
+        where: { email },
       });
 
-      const requiredCreateProfileFields = {
-        fullName: {
-          value: userInput.fullName,
-          visibleTo: PrivacyOption.Friends,
-        },
-        tdaGradYearBannerVisible: {
-          value: false,
-          visibleTo: PrivacyOption.Friends,
-        },
-      };
-
-      // creates a profile for the new user with or without the pronouns field
-      if (userInput.pronouns === undefined || userInput.pronouns === null) {
-        await this.profileService.createProfile(user.id, {
-          ...requiredCreateProfileFields,
-        });
+      if (existingUser) {
+        throw new Error("Email already in use.");
       } else {
-        await this.profileService.createProfile(user.id, {
-          ...requiredCreateProfileFields,
-          pronouns: {
-            value: userInput.pronouns,
-            visibleTo: PrivacyOption.Friends,
+        const password = await this.authService.createPassword(
+          userInput.password
+        );
+
+        // returns the newly created user object to the resolver
+        const user = await this.databaseService.users.create({
+          data: {
+            email: userInput.email,
+            passwordHash: password,
+            isActive: true,
           },
         });
+
+        const requiredCreateProfileFields = {
+          fullName: {
+            value: userInput.fullName,
+            visibleTo: PrivacyOption.Friends,
+          },
+          tdaGradYearBannerVisible: {
+            value: false,
+            visibleTo: PrivacyOption.Friends,
+          },
+        };
+
+        // creates a profile for the new user with or without the pronouns field
+        if (userInput.pronouns === undefined || userInput.pronouns === null) {
+          await this.profileService.createProfile(user.id, {
+            ...requiredCreateProfileFields,
+          });
+        } else {
+          await this.profileService.createProfile(user.id, {
+            ...requiredCreateProfileFields,
+            pronouns: {
+              value: userInput.pronouns,
+              visibleTo: PrivacyOption.Friends,
+            },
+          });
+        }
+        return user;
       }
-      return user;
+    } catch (error) {
+      throw new Error((error as Error).message);
     }
   }
 
   async deactivateAccount(email: string): Promise<AccountResponse> {
-    const user = await this.databaseService.users.findUnique({
-      where: { email },
-    });
+    try {
+      const user = await this.databaseService.users.findUnique({
+        where: { email },
+      });
 
-    if (!user) {
-      throw new Error("User not found");
+      if (!user) {
+        throw new Error("User not found");
+      }
+
+      if (!user.isActive) {
+        throw new Error("User is already inactive");
+      }
+
+      await this.databaseService.users.update({
+        where: { email },
+        data: { isActive: false },
+      });
+
+      // Set a timer to delete the account after 30 days
+      setTimeout(async () => {
+        await this.databaseService.users.delete({ where: { email } });
+      }, 30 * 24 * 60 * 60 * 1000);
+
+      const deactivateAccountResponse: AccountResponse = {
+        statusCode: 200,
+        message:
+          "Your account has been deactivated and will be deleted in 30 days",
+      };
+
+      return deactivateAccountResponse;
+    } catch (error) {
+      return {
+        statusCode: 500,
+        message: (error as Error).message,
+      };
     }
-
-    if (!user.isActive) {
-      throw new Error("User is already inactive");
-    }
-
-    await this.databaseService.users.update({
-      where: { email },
-      data: { isActive: false },
-    });
-
-    // Set a timer to delete the account after 30 days
-    setTimeout(async () => {
-      await this.databaseService.users.delete({ where: { email } });
-    }, 30 * 24 * 60 * 60 * 1000);
-
-    const deactivateAccountResponse: AccountResponse = {
-      statusCode: 200,
-      message:
-        "Your account has been deactivated and will be deleted in 30 days",
-    };
-
-    return deactivateAccountResponse;
   }
 
   async deleteAccount(id: number): Promise<AccountResponse> {
