ecd6c776c956140ed2d21ac3c2f4a4e861f17724
RGibbs96 <106614039+RGibbs96@users.noreply.github.com>
2023-06-05 09:11:42 -0700

    some additional files that did not get added

--- src/modules/profile/profile.service.ts
+++ src/modules/profile/profile.service.ts
@@ -55,56 +55,58 @@ export default class ProfileService {
   ): Promise<Profile | undefined> {
     // ! in here, if profilePic is a field being updated, DO SOME WORK, then we need to call the image service to either create a new profile image, update the image in S3, or delete
     // ! are we storing a url reference in the DB or do we need to call getImage everytime the user profile is resolved?
-    console.log(profileUpdateInput);
+    console.log(profileUpdateInput, "in profile.service");
     if (
       // Delete is the only time that the image service needs to be called
       "profilePic" in profileUpdateInput &&
       profileUpdateInput["profilePic"] == null
     ) {
+      console.log("inside delete");
       const response = this.imageService.deleteImageByUserId(userId);
       console.log(response);
-      const {
-        fullName,
-        pronouns,
-        tdaGradYear,
-        currentLocation,
-        bio,
-        profilePic,
-        homeCountry,
-        nickname,
-        namePronunciation,
-        website,
-        tdaGradYearBannerVisible,
-      } = profileUpdateInput;
-
-      const existingProfile = await prisma.profile.findUnique({
-        where: { userId },
-      });
-
-      if (!existingProfile) {
-        throw new Error("Profile not found");
-      }
-
-      const profile = await prisma.profile.update({
-        where: { userId },
-        data: {
-          fullName: fullName as unknown as Prisma.JsonObject,
-          pronouns: pronouns as unknown as Prisma.JsonObject,
-          tdaGradYear: tdaGradYear as unknown as Prisma.JsonObject,
-          currentLocation: currentLocation as unknown as Prisma.JsonObject,
-          bio: bio as unknown as Prisma.JsonObject,
-          tdaGradYearBannerVisible:
-            tdaGradYearBannerVisible as unknown as Prisma.JsonObject,
-          profilePic: profilePic as unknown as Prisma.JsonObject,
-          homeCountry: homeCountry as unknown as Prisma.JsonObject,
-          nickname: nickname as unknown as Prisma.JsonObject,
-          namePronunciation: namePronunciation as unknown as Prisma.JsonObject,
-          website: website as unknown as Prisma.JsonObject,
-        },
-      });
-
-      return profile;
     }
+    const {
+      fullName,
+      pronouns,
+      tdaGradYear,
+      currentLocation,
+      bio,
+      profilePic,
+      homeCountry,
+      nickname,
+      namePronunciation,
+      website,
+      tdaGradYearBannerVisible,
+    } = profileUpdateInput;
+
+    const existingProfile = await prisma.profile.findUnique({
+      where: { userId },
+    });
+    console.log(existingProfile, "hello");
+
+    if (!existingProfile) {
+      throw new Error("Profile not found");
+    }
+
+    const profile = await prisma.profile.update({
+      where: { userId },
+      data: {
+        fullName: fullName as unknown as Prisma.JsonObject,
+        pronouns: pronouns as unknown as Prisma.JsonObject,
+        tdaGradYear: tdaGradYear as unknown as Prisma.JsonObject,
+        currentLocation: currentLocation as unknown as Prisma.JsonObject,
+        bio: bio as unknown as Prisma.JsonObject,
+        tdaGradYearBannerVisible:
+          tdaGradYearBannerVisible as unknown as Prisma.JsonObject,
+        profilePic: profilePic as unknown as Prisma.JsonObject,
+        homeCountry: homeCountry as unknown as Prisma.JsonObject,
+        nickname: nickname as unknown as Prisma.JsonObject,
+        namePronunciation: namePronunciation as unknown as Prisma.JsonObject,
+        website: website as unknown as Prisma.JsonObject,
+      },
+    });
+
+    return profile;
   }
 
   async getS3Url(userId: number) {
